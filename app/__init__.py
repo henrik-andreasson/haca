aea860f0b4745b3cfa0564dd0652611b765f14d4
Henrik Andreasson <henrik.andreasson@cgi.com>
2024-01-27 23:19:26 +0100

    remove msearch

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 #from app.main.models import models
-from flask_msearch import Search
 import logging
 from logging.handlers import SMTPHandler, RotatingFileHandler
 import os
@@ -26,10 +25,6 @@ babel = Babel()
 
 from app.main.models import Audit
 
-# audit = Audit()
-
-search = Search()
-
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -45,7 +40,6 @@ def create_app(config_class=Config):
     bootstrap.init_app(app)
     moment.init_app(app)
     babel.init_app(app)
-    search.init_app(app)
 
     from app.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
